891175e52b103757c87967581b89e43afe76a171
zvecr <git@zvecr.com>
2022-01-19 23:50:53 +0000

    clang

--- drivers/gpio/pca9555.h
+++ drivers/gpio/pca9555.h
@@ -69,8 +69,8 @@ bool pca9555_set_config(uint8_t slave_addr, pca9555_port_t port, uint8_t conf);
 bool pca9555_set_output(uint8_t slave_addr, pca9555_port_t port, uint8_t conf);
 
 /**
- * Write high/low to both ports sequentially 
- * 
+ * Write high/low to both ports sequentially
+ *
  *  - slightly faster than multiple set_output
  */
 bool pca9555_set_output_all(uint8_t slave_addr, uint8_t confA, uint8_t confB);
@@ -82,7 +82,7 @@ bool pca9555_readPins(uint8_t slave_addr, pca9555_port_t port, uint8_t* ret);
 
 /**
  * Read state of both ports sequentially
- * 
+ *
  *  - slightly faster than multiple readPins
  */
 bool pca9555_readPins_all(uint8_t slave_addr, uint16_t* ret);
